e81edaed25f2887d43b5a8fa60ed0fce1dca7ab4
Mohammed Hayder <mr.hamood277@gmail.com>
2023-02-17 12:49:53 +0300

    changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'next-themes'
-import FileUploadContextProvider from 'contexts/FileUploadContext'
-import ToppingsContextProvider from 'contexts/ToppingsContext'
-import CartContextProvider from 'contexts/CartContext'
-import TagsContextProvider from 'contexts/TagsContext'
-import SearchContextProvider from 'contexts/SearchContext'
-import DashboardOrderContextProvider from 'contexts/DashboardOrderContext'
+import FileUploadContextProvider from '@contexts/FileUploadContext'
+import ToppingsContextProvider from '@contexts/ToppingsContext'
+import CartContextProvider from '@contexts/CartContext'
+import TagsContextProvider from '@contexts/TagsContext'
+import SearchContextProvider from '@contexts/SearchContext'
+import DashboardOrderContextProvider from '@contexts/DashboardOrderContext'
 import { PayPalScriptProvider } from '@paypal/react-paypal-js'
 
 export default function App({ Component, pageProps }: AppProps) {
