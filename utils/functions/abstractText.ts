4909d92e54eee5d2a9a451ec5f1705b110b5bd15
Mohammed Hayder <mr.hamood277@gmail.com>
2023-02-18 08:26:52 +0300

    Versioning untracked files

--- utils/functions/abstractText.ts
+++
@@ -1,4 +0,0 @@
-const abstractText = (txt: string, txtLength: number) =>
-  txt?.length > txtLength ? txt.slice(0, txtLength) + '...' : txt
-
-export default abstractText
--- utils/functions/jsonTools.ts
+++
@@ -1,3 +0,0 @@
-export const parseJson = (jsonString: string) => JSON.parse(jsonString)
-export const stringJson = (jsonObject: object | string) => JSON.stringify(jsonObject)
-export const toJson = (jsObject: Response) => jsObject.json()
