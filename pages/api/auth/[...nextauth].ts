0ac351a2bc1f27e82c28395bb898001a07110f67
Mohammed Hayder <mr.hamood277@gmail.com>
2023-02-18 20:58:39 +0300

    changes

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -11,15 +11,14 @@ export const authOptions = {
       clientSecret: GOOGLE_CLIENT_SECRET!
     })
   ],
-  secret: NEXTAUTH_SECRET,
-  callbacks: {
-    async signIn({ account, profile }: any) {
-      console.log({ account, profile })
-      if (account.provider === 'google') {
-        return profile.email_verified
-      }
-    }
-  }
+  secret: NEXTAUTH_SECRET
+  // ,callbacks: {
+  //   async signIn({ account, profile }: any) {
+  //     if (account.provider === 'google') {
+  //       return profile.email_verified
+  //     }
+  //   }
+  // }
 }
 
 export default NextAuth(authOptions)
