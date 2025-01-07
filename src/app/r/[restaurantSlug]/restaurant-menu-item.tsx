c435302e4c113e54a835b1a262e0836bbb9c41e8
Mohammed Haydar <mr.hamood277@gmail.com>
2025-01-07 14:07:58 +0000

    changed menu item card to be a dialog for better UX in Restaurant page

--- src/app/r/[restaurantSlug]/restaurant-menu-item.tsx
+++ src/app/r/[restaurantSlug]/restaurant-menu-item.tsx
@@ -3,11 +3,20 @@
 import Image from "next/image"
 import { useState } from "react"
 import { Button } from "@/components/ui/button"
-import { Card, CardContent, CardHeader } from "@/components/ui/card"
 import { Checkbox } from "@/components/ui/checkbox"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
 import { useCart } from "@/hooks/use-cart"
 import { useToast } from "@/hooks/use-toast"
 import { DEFAULT_CURRENCY_SYMBOL } from "@/lib/constants"
+import { truncate } from "@/lib/truncate"
 import type { MenuItems } from "@/server/db/schema"
 
 type RestaurantMenuItemProps = {
@@ -45,54 +54,83 @@ export default function RestaurantMenuItem({ item, vendor }: RestaurantMenuItemP
   }
 
   return (
-    <Card className="overflow-clip h-full flex flex-col select-none">
-      <div className="relative h-48 max-h-48 w-full overflow-y-clip">
-        <Image
-          src={item.image}
-          alt={item.name}
-          width={300}
-          height={192}
-          className="w-full object-contain"
-        />
-      </div>
-      <CardHeader>
-        <h4 className="font-semibold">{item.name}</h4>
-        <p className="text-sm text-muted-foreground">
-          {DEFAULT_CURRENCY_SYMBOL}
-          {Number(item.price).toFixed(2)}
-        </p>
-      </CardHeader>
-      <CardContent className="p-3 pt-0 flex flex-col flex-1">
-        {item.addons && item.addons.length > 0 && (
-          <div className="mb-4">
-            <p className="mb-2 text-sm font-medium">Add-ons:</p>
-            {item.addons.map(addon => (
-              <label key={addon.toppingName} className="flex items-center gap-2">
-                <Checkbox
-                  id="addon-checkbox"
-                  checked={selectedAddons[item.id]?.includes(addon.toppingName)}
-                  onCheckedChange={e => {
-                    const current = selectedAddons[item.id] ?? []
-                    setSelectedAddons({
-                      ...selectedAddons,
-                      [item.id]: e.valueOf()
-                        ? [...current, addon.toppingName]
-                        : current.filter(name => name !== addon.toppingName),
-                    })
-                  }}
-                />
-                <span className="text-sm">
-                  {addon.toppingName} (+{DEFAULT_CURRENCY_SYMBOL}
-                  {addon.toppingPrice.toFixed(2)})
-                </span>
-              </label>
-            ))}
+    <Dialog>
+      <DialogTrigger asChild>
+        <div className="relative h-48 max-h-48 w-full overflow-y-clip cursor-pointer">
+          <Image
+            src={item.image}
+            alt={item.name}
+            width={300}
+            height={192}
+            className="w-full h-full object-cover rounded-md shadow-sm hover:shadow-lg transition-shadow"
+          />
+          <div
+            className={
+              "absolute bottom-0 w-full px-2.5 py-3.5 bg-gradient-to-t from-black/90 via-black/50 to-transparent rounded-md"
+            }
+          >
+            <h1 className="text-gray-200 text-lg font-extrabold drop-shadow-md">
+              {truncate(item.name)}
+            </h1>
+            <span className="text-green-600">
+              <strong>{DEFAULT_CURRENCY_SYMBOL}</strong>
+              {Number(item.price).toFixed(2)}
+            </span>
           </div>
-        )}
-        <Button onClick={() => handleAddToCart(item)} className="w-full mt-auto">
-          Add to Cart
-        </Button>
-      </CardContent>
-    </Card>
+        </div>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>{item.name}</DialogTitle>
+          <DialogDescription>
+            <div className="flex flex-col gap-2 justify-between">
+              <strong className="text-sm text-muted-foreground">
+                {DEFAULT_CURRENCY_SYMBOL}
+                {Number(item.price).toFixed(2)}
+              </strong>
+              <p>{item.description}</p>
+            </div>
+            <Image
+              src={item.image}
+              alt={item.name}
+              width={300}
+              height={192}
+              className="w-full max-h-72 object-cover rounded-md shadow mt-2"
+            />
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter className="!flex-col">
+          {item.addons && item.addons.length > 0 && (
+            <div className="flex flex-col">
+              <p className="mb-2 text-sm font-medium">Add-ons:</p>
+              {item.addons.map(addon => (
+                <label key={addon.toppingName} className="flex items-center gap-2">
+                  <Checkbox
+                    id="addon-checkbox"
+                    checked={selectedAddons[item.id]?.includes(addon.toppingName)}
+                    onCheckedChange={e => {
+                      const current = selectedAddons[item.id] ?? []
+                      setSelectedAddons({
+                        ...selectedAddons,
+                        [item.id]: e.valueOf()
+                          ? [...current, addon.toppingName]
+                          : current.filter(name => name !== addon.toppingName),
+                      })
+                    }}
+                  />
+                  <span className="text-sm">
+                    {addon.toppingName} (+{DEFAULT_CURRENCY_SYMBOL}
+                    {addon.toppingPrice.toFixed(2)})
+                  </span>
+                </label>
+              ))}
+            </div>
+          )}
+          <Button onClick={() => handleAddToCart(item)} className="w-full mt-3">
+            Add to Cart
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   )
 }
