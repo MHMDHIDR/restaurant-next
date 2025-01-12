86b5405f2d0c2c352a1702971ad0fe3f82a542a3
Mohammed Haydar <mr.hamood277@gmail.com>
2025-01-12 19:02:46 +0000

    Enhance RestaurantPage with role-based edit link rendering
    - Introduced role access checks for displaying the edit link on the RestaurantPage.
    - Added support for SUPER_ADMIN role to access vendor details for editing.
    - Refactored conditional rendering logic to improve clarity and maintainability.

--- src/app/r/[restaurantSlug]/page.tsx
+++ src/app/r/[restaurantSlug]/page.tsx
@@ -6,7 +6,9 @@ import { notFound } from "next/navigation"
 import EmptyState from "@/components/custom/empty-state"
 import RestaurantMenuItem from "@/components/custom/restaurant-menu-item"
 import { env } from "@/env"
+import { checkRoleAccess } from "@/lib/check-role-access"
 import { auth } from "@/server/auth"
+import { UserRole } from "@/server/db/schema"
 import { api } from "@/trpc/server"
 import type { Vendors } from "@/server/db/schema"
 import type { Metadata } from "next"
@@ -51,6 +53,7 @@ export default async function RestaurantPage({
   const { restaurantSlug } = await params
   const session = await auth()
   const user = session?.user
+  const ALLOWED_ROLES = [UserRole.SUPER_ADMIN] as const
 
   const vendor = await api.vendor
     .getBySlug({ slug: restaurantSlug, getItems: true })
@@ -74,8 +77,16 @@ export default async function RestaurantPage({
         <div className="absolute inset-0 bg-gradient-to-t from-white dark:from-black via-white/75 dark:via-black/75 to-transparent" />
         <div className="container relative h-full px-2 mx-auto max-w-screen-xl">
           <div className="absolute bottom-8">
-            {user?.id === vendor.addedById ? (
+            {user?.id === vendor.addedById &&
+            checkRoleAccess(user?.role, [UserRole.VENDOR_ADMIN]) ? (
               <Link href="/vendor-manager/vendor-details" title={`Edit ${vendor.name} Details`}>
+                <h1 className="mb-4 text-3xl font-bold leading-loose md:leading-10 select-none hover:text-gray-300">
+                  {vendor.name}
+                  <Edit className="inline-flex w-5 h-5 ml-2" />
+                </h1>
+              </Link>
+            ) : checkRoleAccess(user?.role, ALLOWED_ROLES) ? (
+              <Link href={`/dashboard/vendors/${vendor.id}`} title={`Edit ${vendor.name} Details`}>
                 <h1 className="mb-4 text-3xl font-bold leading-loose md:leading-10 select-none hover:text-gray-300">
                   {vendor.name}
                   <Edit className="inline-flex w-5 h-5 ml-2" />
