a4f73072eaadb0e8270799e80e8e2c0ee3d074cc
Mohammed Haydar <mr.hamood277@gmail.com>
2025-01-12 17:49:02 +0000

    fixed theme mode view for the restaurant coverImage

--- src/app/r/[restaurantSlug]/page.tsx
+++ src/app/r/[restaurantSlug]/page.tsx
@@ -58,7 +58,7 @@ export default async function RestaurantPage({
 
   return (
     <div>
-      <section className="relative w-full h-[300px] mb-20">
+      <section className="relative w-full h-72 mb-20">
         <Image
           src={vendor.coverImage}
           alt={`${vendor.name} cover`}
@@ -66,10 +66,10 @@ export default async function RestaurantPage({
           className="object-cover fixed"
           priority
         />
-        <div className="absolute inset-0 bg-gradient-to-t from-black/90 via-black/70 to-transparent" />
+        <div className="absolute inset-0 bg-gradient-to-t from-white dark:from-black via-white/75 dark:via-black/75 to-transparent" />
         <div className="container relative h-full px-2 mx-auto max-w-screen-xl">
           <div className="absolute bottom-8">
-            <h1 className="mb-4 text-3xl font-bold text-white leading-loose md:leading-10 select-none">
+            <h1 className="mb-4 text-3xl font-bold leading-loose md:leading-10 select-none">
               {vendor.name}
             </h1>
             <div className="flex items-center gap-3">
@@ -82,7 +82,7 @@ export default async function RestaurantPage({
                   className="object-contain shadow-md rounded-md"
                 />
               </div>
-              <div className="select-none text-white">
+              <div className="select-none">
                 <p className="opacity-90">{vendor.cuisineTypes.join(", ")}</p>
                 <p className="opacity-90">{vendor.description}</p>
               </div>
